efb92680e22657ff2aea7d8945d6493ed16f823a
Miguel Boland <migueldboland@gmail.com>
2022-10-14 12:21:55 +0100

    Fixed model test

--- test/test_model.py
+++ test/test_model.py
@@ -2,7 +2,7 @@ from model.model import load_new_model
 
 
 def test_load_new_model_should_compile():
-    model = load_new_model(31)
+    model = load_new_model(31, lr=0.01)
     model.compile()
     print(model.summary())
 
