cf25f4ff34796f29adc85e080aab3fa84bc584cb
Miguel Boland <migueldboland@gmail.com>
2022-10-14 13:06:42 +0100

    Added norm_coords_testing

--- test/test_datasets.py
+++ test/test_datasets.py
@@ -1,21 +1,25 @@
-from data.datasets import frame_xy_to_polar
+from data.datasets import norm_coords
 import pytest
 import numpy as np
 
-@pytest.mark.parametrize("xy_coords, polar_coords", [
-    ([[100, 100]], [[0, 0]]),
-    ([[50, 50]], [[0.5, -3/8]]),
-    ([[150, 50]], [[0.5, -1/8]]),
-    ([
-        [100, 100],
-        [50, 50]
-    ], [
-        [0, 0],
-        [0.5, -3/8]
-    ])
+@pytest.mark.parametrize("xy_coords, frame_dim, norm", [
+    ([[0, 0]], [200, 200], [[0, 0]]),
+    ([[100, 100]], [200, 200], [[0.5, 0.5]]),
+    ([[50, 50]], [200, 200], [[0.25, 0.25]]),
+    ([[150, 50]], [200, 200], [[0.75, 0.25]]),
+    ([[150, 50]], [200, 100], [[0.75, 0.5]]),
+    ([[50, 150]], [100, 200], [[0.5, 0.75]]),
 ])
-def test_frame_xy_to_polar_should_return_correct_coords(xy_coords, polar_coords):
-    frame_width = 200
-    frame_height = 200
-    
-    np.testing.assert_almost_equal(frame_xy_to_polar(np.array(xy_coords), frame_height, frame_width), np.array(polar_coords))
+def test_norm_coords_should_norm_coords(xy_coords, frame_dim, norm):
+    frame_height, frame_width = frame_dim
+    np.testing.assert_almost_equal(norm_coords(np.array(xy_coords), frame_height, frame_width), np.array(norm))
+
+@pytest.mark.parametrize("xy_coords, frame_dim, norm", [
+    ([[-1, -1]], [200, 200], [[0, 0]]),
+    ([[300, 300]], [200, 200], [[0.5, 0.5]]),
+])
+def test_norm_coords_should_raise_error_if_invalid_coords(xy_coords, frame_dim, norm):
+    frame_height, frame_width = frame_dim
+    with pytest.raises(AssertionError):
+        np.testing.assert_almost_equal(norm_coords(np.array(xy_coords), frame_height, frame_width), np.array(norm))
+
