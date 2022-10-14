c37db0f0bf7fbbfc5ee70c3b2fb360634e43fed2
Miguel Boland <migueldboland@gmail.com>
2022-10-14 14:20:12 +0100

    Added model saving / reloading

--- test/test_model.py
+++ test/test_model.py
@@ -1,5 +1,11 @@
-from model.model import load_new_model
+from model.model import load_new_model, save_model, load_trained_model
 
+def test_save_and_load_model():
+    mname='test'
+    model = load_new_model(31, lr=0.01)
+    model.compile()
+    save_model(model, mname)
+    model = load_trained_model(mname)
 
 def test_load_new_model_should_compile():
     model = load_new_model(31, lr=0.01)
