ecb8ab921a56d86371ea4e728757a11e82083139
Miguel Boland <migueldboland@gmail.com>
2022-12-31 16:57:39 +0000

    Working baseline model (same performance as non-kedro/mlflow version)

---
+++ smlm-z/src/smlm_z/pipelines/classify/pipeline.py
@@ -0,0 +1,43 @@
+"""
+This is a boilerplate pipeline 'classify'
+generated using Kedro 0.18.4
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import split_train_val_test, train_classifier, eval_classifier, check_data
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+        node(
+            func=split_train_val_test,
+            inputs=['X', 'y', 'parameters'],
+            outputs=['X_train', 'y_train', 'X_val', 'y_val', 'X_test', 'y_test'],
+            name='split_train_val_test',
+            tags=['training']
+        ),
+        node(
+            func=check_data,
+            inputs=['X_train', 'y_train', 'X_val', 'y_val', 'X_test', 'y_test'],
+            outputs='data_plots',
+            name='check_data',
+            tags=['training'],
+        ),
+        node(
+            func=train_classifier,
+            inputs=['X_train', 'y_train', 'X_val', 'y_val', 'parameters'],
+            outputs=['model', 'training_plot'],
+            name='train_classifier',
+            tags=['training']
+        ),
+        node(
+            func=eval_classifier,
+            inputs=['model', 'X_train', 'y_train', 'X_val', 'y_val', 'X_test', 'y_test'],
+            outputs=['clf_metrics', 'clf_figures'],
+            name='eval_classifier',
+            tags=['training']
+        )
+    ],
+        inputs=['X', 'y'],
+        outputs=['data_plots', 'model', 'clf_metrics', 'clf_figures'],
+    )
