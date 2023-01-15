Fixed coordinate normalisation between train and exp image, added reconstruction of simulated nanopore into pipeline

smlm-z/src/smlm_z/pipelines/classify/nodes.py
@@ -2,27 +2,30 @@
 This is a boilerplate pipeline 'classify'
 generated using Kedro 0.18.4
 """
+from sklearn.metrics import max_error, mean_absolute_error
 from typing import Tuple, Dict
 import numpy as np
 from sklearn.model_selection import train_test_split
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import seaborn as sns
+import operator as op
 
+import tensorflow as tf
 from keras.layers import Input, Dense, Dropout
 from keras.models import Sequential, Model
 from tensorflow.keras import optimizers
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from tqdm.keras import TqdmCallback
-import seaborn as sns
-import operator as op
+
 
 def split_train_val_test(X: Tuple[np.array, np.array], y: np.array, parameters: Dict):
     train_size = parameters['train_size']
     test_size = parameters['test_size']
     idx = np.arange(0, y.shape[0])
-    train_idx, other_idx = train_test_split(idx, train_size=train_size, random_state=42)
+    train_idx, other_idx = train_test_split(
+        idx, train_size=train_size, random_state=parameters['random_seed'])
     val_idx, test_idx = train_test_split(
-        other_idx, test_size=test_size, random_state=42)
+        other_idx, test_size=test_size, random_state=parameters['random_seed'])
 
     X_train = [x[train_idx] for x in X]
     y_train = y[train_idx]
@@ -38,7 +41,7 @@ def split_train_val_test(X: Tuple[np.array, np.array], y: np.array, parameters: 
 
 def compile_model(model, lr):
     model.compile(loss='mean_squared_error', optimizer=optimizers.Adam(
-        learning_rate=lr, decay=1e-6), metrics=['mean_absolute_error'])
+        learning_rate=lr), metrics=['mean_absolute_error'])
     print(model.summary())
 
 
@@ -58,11 +61,11 @@ def get_model(parameters):
         input_shape=img_input_shape
     )
     x = img_input
-    augmentation = Sequential([
-        tf.keras.layers.GaussianNoise(0.2, seed=42),
-        tf.keras.layers.RandomTranslation(1/bound, 1/bound)
-    ])
-    x = augmentation(x)
+    # augmentation = Sequential([
+    # tf.keras.layers.GaussianNoise(0.2, seed=42),
+    # tf.keras.layers.RandomTranslation(1/bound, 1/bound)
+    # ])
+    # x = augmentation(x)
     x = resnet(x)
 
     mlp = Sequential([
@@ -80,17 +83,23 @@ def get_model(parameters):
 
 
 def train_classifier(X_train: Tuple[np.array, np.array], y_train: np.array, X_val: Tuple[np.array, np.array], y_val: np.array, parameters: Dict):
+    tf.keras.utils.set_random_seed(parameters['random_seed'])
+    tf.random.set_seed(parameters['random_seed'])
+
     batch_size = parameters['training']['batch_size']
     epochs = parameters['training']['max_epochs']
     model = get_model(parameters)
     callbacks = [
-        ReduceLROnPlateau(monitor='mean_absolute_error', factor=0.1, patience=25, verbose=True, mode='min', min_delta=1, min_lr=1e-7,),
-        EarlyStopping(monitor='val_mean_absolute_error', patience=300, verbose=False, min_delta=1, restore_best_weights=True),
+        ReduceLROnPlateau(monitor='mean_absolute_error', factor=0.1,
+                          patience=25, verbose=True, mode='min', min_delta=1, min_lr=1e-7,),
+        EarlyStopping(monitor='val_mean_absolute_error', patience=300,
+                      verbose=False, min_delta=1, restore_best_weights=True),
         TqdmCallback(verbose=1),
     ]
 
-    history = model.fit(X_train, y_train, epochs=epochs, shuffle=True, verbose=False, batch_size=batch_size, validation_data=(X_val, y_val), callbacks=callbacks)
-    
+    history = model.fit(X_train, y_train, epochs=1, shuffle=True, verbose=False,
+                        batch_size=batch_size, validation_data=(X_val, y_val), callbacks=callbacks)
+
     plt.rcParams['figure.figsize'] = [10, 5]
     fig, ax1 = plt.subplots()
     ax1.plot(history.history['mean_absolute_error'], label='mse')
@@ -103,7 +112,6 @@ def train_classifier(X_train: Tuple[np.array, np.array], y_train: np.array, X_va
     return model, fig
 
 
-from sklearn.metrics import max_error, mean_absolute_error
 def eval_classifier(model, X_train: Tuple[np.array, np.array], y_train: np.array, X_val: Tuple[np.array, np.array], y_val: np.array, X_test: Tuple[np.array, np.array], y_test: np.array):
     datasets = {
         'train': (X_train, y_train),
@@ -112,6 +120,7 @@ def eval_classifier(model, X_train: Tuple[np.array, np.array], y_train: np.array
     }
     all_metrics = {}
     figs = []
+    error_scatters = []
     for k, (x, y) in datasets.items():
         pred_y = model.predict(x).squeeze()
         metrics = {
@@ -126,11 +135,16 @@ def eval_classifier(model, X_train: Tuple[np.array, np.array], y_train: np.array
         plt.ylabel('pred z [nm]')
         plt.title(k)
         figs.append(fig)
-        
-    
+
+        fig = plt.figure()
+        plt.title(k)
+        sns.scatterplot(x=x[1][:, 0], y=x[1][:, 1], hue=abs(pred_y.squeeze()-y.squeeze()))
+        error_scatters.append(fig)
+
     all_metrics = {k: {'value': v, 'step': 0} for k, v in all_metrics.items()}
 
-    return all_metrics, figs
+    return all_metrics, figs, error_scatters
+
 
 def create_boxplot(data, ylabel):
     fig = plt.figure()
@@ -149,6 +163,7 @@ def create_boxplot(data, ylabel):
     plt.title(ylabel)
     return fig
 
+
 def check_data(X_train, y_train, X_val, y_val, X_test, y_test):
     figs = []
     # z coords
@@ -160,23 +175,23 @@ def check_data(X_train, y_train, X_val, y_val, X_test, y_test):
     figs.append(create_boxplot(z_data, 'z [nm]'))
 
     pixel_data = {
-        'train': X_train[0].mean(axis=(1,2,3)),
-        'val': X_val[0].mean(axis=(1,2,3)),
-        'test': X_test[0].mean(axis=(1,2,3)),
+        'train': X_train[0].mean(axis=(1, 2, 3)),
+        'val': X_val[0].mean(axis=(1, 2, 3)),
+        'test': X_test[0].mean(axis=(1, 2, 3)),
     }
     figs.append(create_boxplot(pixel_data, 'mean pixel val'))
 
     pixel_data = {
-        'train': X_train[0].max(axis=(1,2,3)),
-        'val': X_val[0].max(axis=(1,2,3)),
-        'test': X_test[0].max(axis=(1,2,3)),
+        'train': X_train[0].max(axis=(1, 2, 3)),
+        'val': X_val[0].max(axis=(1, 2, 3)),
+        'test': X_test[0].max(axis=(1, 2, 3)),
     }
     figs.append(create_boxplot(pixel_data, 'max pixel val'))
 
     pixel_data = {
-        'train': X_train[0].min(axis=(1,2,3)),
-        'val': X_val[0].min(axis=(1,2,3)),
-        'test': X_test[0].min(axis=(1,2,3)),
+        'train': X_train[0].min(axis=(1, 2, 3)),
+        'val': X_val[0].min(axis=(1, 2, 3)),
+        'test': X_test[0].min(axis=(1, 2, 3)),
     }
     figs.append(create_boxplot(pixel_data, 'min pixel val'))
 
@@ -196,5 +211,31 @@ def check_data(X_train, y_train, X_val, y_val, X_test, y_test):
     }
     figs.append(create_boxplot(theta_data, 'theta'))
 
-
     return figs
+
+
+def augment_psf(psf, parameters):
+    return psf + np.random.normal(0, parameters['data_augmentation']['noise_std'], size=psf.shape)
+
+
+def augment_datasets(X_train: Tuple[np.array, np.array], y_train: np.array, parameters: Dict):
+    np.random.seed(parameters['random_seed'])
+
+    n_aug = y_train.shape[0] * parameters['training']['aug_ratio']
+
+    x_psfs, x_coords = X_train
+
+    aug_psfs = np.zeros((n_aug, *x_psfs.shape[1:]))
+    aug_coords = np.zeros((n_aug, *x_coords.shape[1:]))
+    aug_zs = np.zeros((n_aug, 1))
+
+    for store_i, select_i in enumerate(np.random.randint(0, y_train.shape[0], size=n_aug)):
+        psf = augment_psf(x_psfs[select_i], parameters)
+        aug_psfs[store_i] = psf
+        aug_coords[store_i] = x_coords[select_i]
+        aug_zs[store_i] = y_train[select_i]
+
+    all_psfs = np.concatenate((x_psfs, aug_psfs))
+    all_coords = np.concatenate((x_coords, aug_coords))
+    all_zs = np.concatenate((y_train, aug_zs))
+    return (all_psfs, all_coords), all_zs

smlm-z/src/smlm_z/pipelines/preprocessing/nodes.py
@@ -2,6 +2,7 @@
 This is a boilerplate pipeline 'preprocessing'
 generated using Kedro 0.18.4
 """
+from .new_align import get_offsets
 from typing import Dict
 
 import numpy as np
@@ -10,9 +11,8 @@ from skimage.transform import resize
 import tensorflow as tf
 from skimage.feature import match_template
 from .align_psfs import classic_align_psfs
-
-DEBUG = False
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def get_spot_corner_coords(loc, spot_size):
     x, y = loc['x'], loc['y']
@@ -21,11 +21,12 @@ def get_spot_corner_coords(loc, spot_size):
     return x0, y0
 
 
-def extract_training_stacks(spots: np.array, bead_stack: np.array, picasso_params: Dict) -> np.array:
+def extract_training_stacks(spots: np.array, bead_stack: np.array, parameters: Dict) -> np.array:
+    picasso_params = parameters['picasso']
     spot_size = picasso_params['spot_size']
     frame = bead_stack[20]
     stacks = []
-    if DEBUG:
+    if parameters['DEBUG']:
         spots = spots[0:3]
     for spot in spots:
         res = match_template(frame, spot)
@@ -36,21 +37,31 @@ def extract_training_stacks(spots: np.array, bead_stack: np.array, picasso_param
 
 
 def resize_stacks(stacks: np.array, target_shape: Dict) -> np.array:
-    stacks = np.array([resize(s, [s.shape[0]] + target_shape) for s in stacks])
+    stacks = np.array([resize(s, target_shape) for s in stacks])
     return stacks
 
 
-def align_stacks(bead_stacks: np.array, locs: pd.DataFrame):
-    return classic_align_psfs(bead_stacks, locs)
+def align_stacks(bead_stacks: np.array, locs: pd.DataFrame, parameters: Dict):
+    offsets = classic_align_psfs(bead_stacks, locs)
+    # offsets = get_offsets(locs, bead_stacks)
+    fig = plt.figure()
+    z_step = parameters['images']['train']['z_step']
+    sns.scatterplot(data=locs, x='x', y='y', hue=(offsets - offsets.min()) * z_step)
+    plt.ylabel('y [nm]')
+    plt.xlabel('x [nm]')
+    return offsets, fig
 
 
-def norm_coordinates(locs: pd.DataFrame, src_img: np.array) -> pd.DataFrame:
-    if DEBUG:
+def norm_coordinates(locs: pd.DataFrame, parameters: np.array) -> pd.DataFrame:
+    if parameters['DEBUG']:
         locs = locs.iloc[0:3]
     xy = locs[['x', 'y']]
-    img_y, img_x = src_img.shape[1:]
-    xy['y'] = xy['y'] / img_y
-    xy['x'] = xy['x'] / img_x
+    img_y, img_x = parameters['images']['train']['shape'][1:]
+    xy['y'] = xy['y'] - (img_y/2)
+    xy['x'] = xy['x'] - (img_x/2)
+
+    xy['y'] /= img_y
+    xy['x'] /= img_x
     return xy
 
 
@@ -68,7 +79,7 @@ def stacks_to_training_data(stacks: np.array, norm_coords: pd.DataFrame, offsets
     psfs = np.concatenate(psfs)
     xy_coords = np.concatenate(xy_coords)
     z_coords = np.concatenate(z_coords)[:, np.newaxis]
-    return (psfs, xy_coords), z_coords
+    return psfs, xy_coords, z_coords
 
 
 def norm_zero_one(img):
@@ -83,3 +94,7 @@ def norm_images(psfs: np.array):
         stack = np.stack([norm_zero_one(psf) for psf in stack])
         norm_stacks.append(stack)
     return np.stack(norm_stacks)
+
+
+def merge_model_inputs(resized_psfs, xy_coords, z_coords):
+    return (resized_psfs, xy_coords), z_coords