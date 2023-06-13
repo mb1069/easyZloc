Fixed image normalisation, reverted away from PSF modelling as z-alignment seems to account for low % of error

data/align_psfs.py
@@ -126,7 +126,7 @@ def pad_and_fit_spline(coords, psf, z, z_ups):
     return x, y, cs(z_ups)
     
 def upsample_psf(psf, ratio=UPSCALE_RATIO):
-    pad_width = 5
+    pad_width = 10
     z = np.arange(-pad_width, psf.shape[0] + pad_width)
     z_ups = np.arange(0, psf.shape[0], 1/ratio)
     upsampled_psf = np.zeros((z_ups.shape[0], *psf.shape[1:]))
@@ -236,6 +236,30 @@ def get_or_prepare_psf(prepped_psfs, raw_psfs, idx):
         prepped_psfs[idx] = prepare_psf(raw_psfs[idx])
     return prepped_psfs[idx]
 
+
+def debug_psfs(psf, ref_psf, offset):
+
+
+    ref_psf[:, :, 0] = np.max(ref_psf)
+    ref_psf[:, :, -1] = np.max(ref_psf)
+    ref_psf[:, 0, :] = np.max(ref_psf)
+    ref_psf[:, -1, :] = np.max(ref_psf)
+    ref_psf = ref_psf[::UPSCALE_RATIO]
+    psf = psf[::UPSCALE_RATIO]
+
+    z_ref = np.arange(0, ref_psf.shape[0])
+    z2 = z_ref - offset
+    zs = np.concatenate((z_ref, z2))
+    imgs = np.concatenate((ref_psf, psf))
+
+    idx = np.argsort(zs)
+    imgs = imgs[idx]
+    plt.imshow(grid_psfs(imgs))
+    plt.show()
+
+
+
+
 def align_psfs(psfs, df):
     print(f'Aligning {psfs.shape} psfs...')
 
@@ -249,8 +273,6 @@ def align_psfs(psfs, df):
     for i in trange(1, psfs.shape[0]):
         psf = psfs[i]
         psf = prepare_psf(psf)
-        print(psf.min(), psf.max())
-        print(ref_psf.min(), ref_psf.max())
         psf = match_histograms(psf, ref_psf)
         offset = tf_find_optimal_roll(ref_psf, psf)
         offsets[i] = offset

data/datasets.py
@@ -199,14 +199,20 @@ def fake_psfs():
 
 
 class TrainingPicassoDataset(PicassoDataset):
-    def __init__(self, config, z_range=1000, raw_data_only=False, lazy=False, add_noise=True):
+    def __init__(self, config, z_range=1000, raw_data_only=False, lazy=False, add_noise=True, split_datasets=True):
         super().__init__(config)
+        # self.spots = self.spots[0:1]
+        # self.csv_data = self.csv_data.iloc[0:1]
+        # self.spots = np.concatenate([self.spots]*10)
+        # print(self.spots.shape)
+        # self.csv_data = pd.concat([self.csv_data]*10)
+        
+        self.split_datasets = split_datasets
         self.add_noise = add_noise
         self.z_range = z_range
         self.raw_data_only = raw_data_only
         self.img = imread(os.path.join(config['bpath'], config['img']))
         self.frame = imread(os.path.join(config['bpath'], config['frame']))
-        print(self.img.shape, self.frame.shape)
         assert self.img.shape[1:] == self.frame.shape
 
         if not lazy:
@@ -219,6 +225,7 @@ class TrainingPicassoDataset(PicassoDataset):
         if self.raw_data_only:
             self.data = (psfs, coords), zs
         else:
+
             self.data = self.split_data(psfs, coords, zs)
             if self.add_noise:
                 self.augment_with_noise()
@@ -324,18 +331,24 @@ class TrainingPicassoDataset(PicassoDataset):
         return psfs, coords, zs
 
     def split_data(self, psfs, coords, zs):
-        data = dict()
-        train_size = 0.7
-        val_size = 0.2
-        test_size = 0.1
+        data = {}
 
-        psf_train, psf_other, coords_train, coords_other, zs_train, zs_other = train_test_split(psfs, coords, zs, train_size=train_size, random_state=42)
+        if self.split_datasets:
+            train_size = 0.7
+            val_size = 0.2
+            test_size = 0.1
 
-        psf_val, psf_test, coords_val, coords_test, zs_val, zs_test = train_test_split(psf_other, coords_other, zs_other, train_size=val_size/(val_size+test_size), random_state=42)
+            psf_train, psf_other, coords_train, coords_other, zs_train, zs_other = train_test_split(psfs, coords, zs, train_size=train_size, random_state=42, stratify=zs)
 
-        data['train'] = [[psf_train, coords_train], zs_train]
-        data['val'] = [[psf_val, coords_val], zs_val]
-        data['test'] = [[psf_test, coords_test], zs_test]
+            psf_val, psf_test, coords_val, coords_test, zs_val, zs_test = train_test_split(psf_other, coords_other, zs_other, train_size=val_size/(val_size+test_size), random_state=42, stratify=zs_other)
+
+            data['train'] = [[psf_train, coords_train], zs_train]
+            data['val'] = [[psf_val, coords_val], zs_val]
+            data['test'] = [[psf_test, coords_test], zs_test]
+        else:
+            data['train'] = [[psfs, coords], zs]
+            data['val'] = [[psfs, coords], zs]
+            data['test'] = [[psfs, coords], zs]
         return data
 
     def slice_locs_to_stacks(self):
@@ -1099,12 +1112,6 @@ class StormDataset(ExperimentalDataSet):
         self.xyz_coords = xyz_coords
 
         print(f'Final: images {self.data[0].shape[0]} - DF: {self.csv_data.shape}')
-
-
-
-
-
-
 
     def fetch_all_emitters(self):
         all_psfs = []

smlm-z/src/smlm_z/pipelines/train_model/nodes.py
@@ -11,32 +11,63 @@ import seaborn as sns
 import operator as op
 
 import tensorflow as tf
-from keras.layers import Input, Dense, Dropout
+from keras.layers import Input, Dense, Dropout, Lambda
 from keras.models import Sequential, Model
 from tensorflow.keras import optimizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from tqdm.keras import TqdmCallback
+from smlm_z.extras.visualise import grid_psfs
+
 from ..preprocessing.nodes import norm_images
 
 def split_train_val_test(X: Tuple[np.array, np.array], y: np.array, parameters: Dict):
+    X, y = chunk_data(X, y)
+
     train_size = parameters['train_size']
     test_size = parameters['test_size'] / (parameters['test_size']+parameters['val_size'])
-    idx = np.arange(0, y.shape[0])
+    idx = np.arange(len(y))
     train_idx, other_idx = train_test_split(
         idx, train_size=train_size, random_state=parameters['random_seed'])
     val_idx, test_idx = train_test_split(
         other_idx, test_size=test_size, random_state=parameters['random_seed'])
+    print(np.array(X[0]).shape, np.array(X[1]).shape)
 
-    X_train = [x[train_idx] for x in X]
-    y_train = y[train_idx]
+    X_train = X[train_idx][0], X[train_idx][1]
+    X_val = X[val_idx][0], X[val_idx][1]
+    X_test = X[test_idx][0], X[test_idx][1]
 
-    X_val = [x[val_idx] for x in X]
-    y_val = y[val_idx]
+    y_train = np.stack(y[train_idx])
+    y_val = np.stack(y[val_idx])
+    y_test = np.stack(y[test_idx])
 
-    X_test = [x[test_idx] for x in X]
-    y_test = y[test_idx]
+    print(X_train.shape, y_train.shape)
+    splits = np.zeros(y.shape[0])
+    splits[train_idx] = 0
+    splits[val_idx] = 1
+    splits[test_idx] = 2
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    idx_dict = {0: 'train', 1: 'val', 2: 'test'}
+    func = np.vectorize(lambda v: idx_dict[v])
+    splits = func(splits)
+    return X_train, y_train, X_val, y_val, X_test, y_test, splits
+
+def norm_psfs(X_train, X_val, X_test, X):
+    train_psfs, train_coords = X_train
+    val_psfs, val_coords = X_val
+    test_psfs, test_coords = X_test
+    all_psfs, all_coords = X
+
+    datagen = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
+
+    datagen.fit(train_psfs)
+
+    train_psfs = datagen.standardize(train_psfs)
+    val_psfs = datagen.standardize(val_psfs)
+    test_psfs = datagen.standardize(test_psfs)
+    all_psfs = datagen.standardize(all_psfs)
+
+    return (train_psfs, train_coords), (val_psfs, val_coords), (test_psfs, test_coords), (all_psfs, all_coords)
 
 
 def compile_model(model, lr):
@@ -54,55 +85,65 @@ def get_model(parameters):
 
     weights = 'imagenet' if n_channels == 3 else None
     print(f'Using weights {weights}')
-    resnet = tf.keras.applications.resnet_v2.ResNet101V2(
+    
+    resnet = tf.keras.applications.resnet_v2.ResNet50V2(
         include_top=False,
         weights=weights,
         pooling='max',
-        input_shape=img_input_shape
+        input_shape=img_input_shape,
     )
     x = img_input
-    # augmentation = Sequential([
-    #     tf.keras.layers.GaussianNoise(0.2, seed=42),
-    #     tf.keras.layers.RandomTranslation(1/bound, 1/bound)
-    # ])
-    # x = augmentation(x)
+    augmentation = Sequential([
+        tf.keras.layers.GaussianNoise(0.001, seed=parameters['random_seed']),
+        # tf.keras.layers.RandomTranslation(0.05, 0.05, seed=parameters['random_seed']),
+    ])
+    x = augmentation(x)
     x = resnet(x)
 
-    mlp = Sequential([
-        Dense(1024),
-        Dropout(0.5),
-        Dense(1024),
-        Dropout(0.5),
-        Dense(1)
-    ])
     x = tf.concat((x, coords_input), axis=1)
+
+    mlp = Sequential([
+        Dense(512, activation='relu'),
+        Dropout(0.5),
+        Dense(512, activation='relu'),
+        Dropout(0.5),
+        Dense(1),
+        # Dense(1, activation='sigmoid'),
+        # Lambda(lambda x: ((x-0.5)*2)*parameters['z_range'])
+    ])
+
     x = mlp(x)
     model = Model(inputs=(img_input, coords_input), outputs=x)
+    print(model.summary())
     compile_model(model, lr)
     return model
 
 
-def train_classifier(X_train: Tuple[np.array, np.array], y_train: np.array, X_val: Tuple[np.array, np.array], y_val: np.array, parameters: Dict):
+def train_classifier(X_train: Tuple[np.array, np.array], y_train: np.array, X_val: Tuple[np.array, np.array], y_val: np.array, parameters: Dict, extra_callbacks=None):
     tf.keras.utils.set_random_seed(parameters['random_seed'])
     tf.random.set_seed(parameters['random_seed'])
 
     batch_size = parameters['training']['batch_size']
     epochs = parameters['training']['max_epochs']
     model = get_model(parameters)
-    model = tf.keras.models.load_model('/home/miguel/Projects/uni/phd/smlm_z/smlm-z/data/06_models/warmstart_model')
-    from keras import backend as K
-    K.set_value(model.optimizer.learning_rate, parameters['training']['learning_rate'])
+    # model = tf.keras.models.load_model('/home/miguel/Projects/uni/phd/smlm_z/smlm-z/data/06_models/warmstart_model')
+    # from keras import backend as K
+    # K.set_value(model.optimizer.learning_rate, parameters['training']['learning_rate'])
     
     callbacks = [
         ReduceLROnPlateau(monitor='mean_absolute_error', factor=0.1,
-                          patience=25, verbose=True, mode='min', min_delta=1, min_lr=1e-7,),
-        EarlyStopping(monitor='val_mean_absolute_error', patience=200,
+                          patience=50, verbose=True, mode='min', min_delta=1, min_lr=1e-7,),
+        EarlyStopping(monitor='val_mean_absolute_error', patience=250,
                       verbose=False, min_delta=1, restore_best_weights=True),
         TqdmCallback(verbose=1),
     ]
-
-    history = model.fit(X_train, y_train, epochs=epochs, shuffle=True, verbose=False,
-                        batch_size=batch_size, validation_data=(X_val, y_val), callbacks=callbacks)
+    if extra_callbacks is not None:
+        callbacks += extra_callbacks
+    try:
+        history = model.fit(X_train, y_train, epochs=epochs, shuffle=True, verbose=False,
+                            batch_size=batch_size, validation_data=(X_val, y_val), callbacks=callbacks)
+    except KeyboardInterrupt:
+        history = model.history
 
     plt.rcParams['figure.figsize'] = [10, 5]
     fig, ax1 = plt.subplots()
@@ -116,7 +157,29 @@ def train_classifier(X_train: Tuple[np.array, np.array], y_train: np.array, X_va
     return model, fig
 
 
-def eval_classifier(model, X_train: Tuple[np.array, np.array], y_train: np.array, X_val: Tuple[np.array, np.array], y_val: np.array, X_test: Tuple[np.array, np.array], y_test: np.array):
+def chunk_data(X, y, data_splits=None):
+    idx = np.argwhere(np.diff(y.squeeze())<0).squeeze()
+    idx += 1
+    idx = np.concatenate([[0], idx, [len(y)]])
+    Xs = []
+    ys = []
+    if data_splits is not None:
+        splits = []
+    for i in range(0, len(idx)-1):
+        s = idx[i]
+        end = idx[i+1]
+        Xs.append((X[0][s:end], X[1][s:end]))
+        ys.append(y[s:end])
+        if data_splits is not None:
+            splits.append(data_splits[s:end])
+
+    if data_splits is not None:
+        return Xs, ys, splits
+    return Xs, ys
+
+
+def eval_classifier(model, X, y, X_train: Tuple[np.array, np.array], y_train: np.array, X_val: Tuple[np.array, np.array], y_val: np.array, X_test: Tuple[np.array, np.array], y_test: np.array, data_splits: np.array):
+
     datasets = {
         'train': (X_train, y_train),
         'val': (X_val, y_val),
@@ -125,6 +188,32 @@ def eval_classifier(model, X_train: Tuple[np.array, np.array], y_train: np.array
     all_metrics = {}
     figs = []
     error_scatters = []
+
+    Xs, ys, data_splits = chunk_data(X, y, data_splits)
+    pred_ys = []
+    true_ys = []
+    fig = plt.figure()
+    for i, (X, y, s) in enumerate(zip(Xs, ys, data_splits)):
+        pred_y = model.predict(X).squeeze()
+        pred_ys.append(pred_y)
+        true_ys.append(y)
+
+        non_train_idx = np.argwhere(s!='train').squeeze()
+
+        plt.plot(y, pred_y, label=str(i))
+        non_train_y = y[non_train_idx]
+        non_train_pred_y = pred_y[non_train_idx]
+        plt.scatter(non_train_y, non_train_pred_y)
+        if i % 5 == 4 or (i == len(X)-1):
+            plt.plot([-1000, 1000], [-1000, 1000], 'r--')
+            plt.xlabel('True Z')
+            plt.ylabel('Pred Z')
+            plt.title('Offset checkers')
+            plt.legend()
+            figs.append(fig)
+            fig = plt.figure()
+
+
     for k, (x, y) in datasets.items():
         pred_y = model.predict(x).squeeze()
         metrics = {
@@ -142,13 +231,66 @@ def eval_classifier(model, X_train: Tuple[np.array, np.array], y_train: np.array
 
         fig = plt.figure()
         plt.title(k)
-        sns.scatterplot(x=x[1][:, 0], y=x[1][:, 1], hue=abs(pred_y.squeeze()-y.squeeze()))
+        errors = abs(pred_y.squeeze()-y.squeeze())
+        sns.scatterplot(x=x[1][:, 0], y=x[1][:, 1], hue=errors)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        error_scatters.append(fig)
+
+        # Pixel val / error
+        max_pixel_val = x[0].max(axis=(1, 2, 3))
+        fig = plt.figure()
+        plt.scatter(max_pixel_val, errors)
+        plt.xlabel('max pixel val')
+        plt.ylabel('error')
+        plt.title(k)
+        error_scatters.append(fig)
+
+        # Min val / error
+        min_pixel_val = x[0].min(axis=(1, 2, 3))
+        fig = plt.figure()
+        plt.scatter(min_pixel_val, errors)
+        plt.xlabel('min pixel val')
+        plt.ylabel('error')
+        plt.title(k)
+        error_scatters.append(fig)
+
+        # Mean val / error
+        mean_pixel_val = x[0].mean(axis=(1, 2, 3))
+        fig = plt.figure()
+        plt.scatter(mean_pixel_val, errors)
+        plt.xlabel('min pixel val')
+        plt.ylabel('error')
+        plt.title(k)
+        error_scatters.append(fig)
+        
+        # X
+        x_val = x[1][:, 0]
+        fig = plt.figure()
+        plt.scatter(x_val, errors)
+        plt.xlabel('X')
+        plt.ylabel('error')
+        plt.title(k)
+        error_scatters.append(fig)
+
+        # Y
+        y_val = x[1][:, 1]
+        fig = plt.figure()
+        plt.scatter(y_val, errors)
+        plt.xlabel('Y')
+        plt.ylabel('error')
+        plt.title(k)
         error_scatters.append(fig)
 
     all_metrics = {k: {'value': v, 'step': 0} for k, v in all_metrics.items()}
 
     return all_metrics, figs, error_scatters
 
+def create_hist(data, xlabel):
+    fig = plt.figure()
+    sns.histplot(data, stat='probability', common_norm=False)
+    plt.xlabel(xlabel)
+    return fig
 
 def create_boxplot(data, ylabel):
     fig = plt.figure()
@@ -170,34 +312,52 @@ def create_boxplot(data, ylabel):
 
 def check_data(X_train, y_train, X_val, y_val, X_test, y_test):
     figs = []
+    for k, v in [('train', (X_train, y_train)), ('val', (X_val, y_val)), ('test', (X_test, y_test))]:
+        plt.rcParams["figure.figsize"] = (7, 7)
+        idx = np.random.choice(list(range(len(v[1]))), size=100)
+        idx_imgs = v[0][0][idx]
+        idx_ys = v[1][idx].squeeze()
+        order = np.argsort(idx_ys)
+        imgs = idx_imgs[order].mean(axis=-1)
+        fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2)
+        im = ax0.imshow(grid_psfs(imgs))
+        plt.title(k)
+        fig.colorbar(im)
+
+        ax1.plot(list(range(len(idx_ys))), idx_ys[order])
+
+        figs.append(fig)
+
+
+
     # z coords
     z_data = {
-        'train': y_train,
-        'val': y_val,
-        'test': y_test
+        'train': y_train.squeeze(),
+        'val': y_val.squeeze(),
+        'test': y_test.squeeze()
     }
-    figs.append(create_boxplot(z_data, 'z [nm]'))
+    figs.append(create_hist(z_data, 'z [nm]'))
 
     pixel_data = {
         'train': X_train[0].mean(axis=(1, 2, 3)),
         'val': X_val[0].mean(axis=(1, 2, 3)),
         'test': X_test[0].mean(axis=(1, 2, 3)),
     }
-    figs.append(create_boxplot(pixel_data, 'mean pixel val'))
+    figs.append(create_hist(pixel_data, 'mean pixel val'))
 
     pixel_data = {
         'train': X_train[0].max(axis=(1, 2, 3)),
         'val': X_val[0].max(axis=(1, 2, 3)),
         'test': X_test[0].max(axis=(1, 2, 3)),
     }
-    figs.append(create_boxplot(pixel_data, 'max pixel val'))
+    figs.append(create_hist(pixel_data, 'max pixel val'))
 
     pixel_data = {
         'train': X_train[0].min(axis=(1, 2, 3)),
         'val': X_val[0].min(axis=(1, 2, 3)),
         'test': X_test[0].min(axis=(1, 2, 3)),
     }
-    figs.append(create_boxplot(pixel_data, 'min pixel val'))
+    figs.append(create_hist(pixel_data, 'min pixel val'))
 
     # Coord values
     x_data = {
@@ -205,7 +365,7 @@ def check_data(X_train, y_train, X_val, y_val, X_test, y_test):
         'val': X_val[1][:, 0].flatten(),
         'test': X_test[1][:, 0].flatten(),
     }
-    figs.append(create_boxplot(x_data, 'x'))
+    figs.append(create_hist(x_data, 'x'))
 
     # Coord values
     y_data = {
@@ -213,7 +373,7 @@ def check_data(X_train, y_train, X_val, y_val, X_test, y_test):
         'val': X_val[1][:, 1].flatten(),
         'test': X_test[1][:, 1].flatten(),
     }
-    figs.append(create_boxplot(y_data, 'y'))
+    figs.append(create_hist(y_data, 'y'))
 
     return figs
 
@@ -229,11 +389,15 @@ def random_translate(psf, parameters):
 
 def augment_psf(psf, parameters):
     psf += np.random.normal(0, parameters['data_augmentation']['noise_std'], size=psf.shape)
+    psf[psf<0] = 0
     psf = random_translate(psf, parameters)
     return psf
 
 
 def augment_datasets(X_train: Tuple[np.array, np.array], y_train: np.array, parameters: Dict):
+    # TODO fix augmentation
+    return X_train, y_train
+
     np.random.seed(parameters['random_seed'])
     n_aug = y_train.shape[0] * parameters['training']['aug_ratio']
 