331c7008cee56011efb4e3fd74d8fb147e494b07
Miguel Boland <migueldboland@gmail.com>
2023-12-17 15:04:23 +0000

    Added script to run picasso onn bead stacks

---
+++ publication/prep_training_data/run_picasso.py
@@ -0,0 +1,209 @@
+from argparse import ArgumentParser
+import os
+from glob import glob
+from natsort import natsorted
+import subprocess
+import h5py
+import pandas as pd
+import numpy as np
+from sklearn.metrics import euclidean_distances
+from tifffile import imread, imwrite
+from skimage.feature import match_template
+from skimage.filters import butterworth
+from skimage.filters import gaussian
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+DEBUG = False
+
+def validate_args(args):
+    print(f"Found {len(args['bead_stacks'])} bead stacks")
+    for f in natsorted(args['bead_stacks']):
+        print(f'\t - {f}')
+
+
+def test_picasso_exec():
+    res = subprocess.run(['picasso', '-h'], capture_output=True, text=True)
+    if res.returncode != 0:
+        print(res.stdout)
+        print(res.stderr)
+        print('\n')
+        raise EnvironmentError('Picasso not found/working (see above)')
+
+def transform_args(args):
+    fnames = glob(f"{args['bead_stacks']}/**/*.tif")
+    args['outpath'] = args['bead_stacks']
+    fnames = [os.path.abspath(f) for f in fnames if '_slice.ome.tif' not in f and os.path.basename(f) != 'stacks.ome.tif']
+    args['bead_stacks'] = fnames
+    return args
+
+def get_or_create_slice(bead_stack, slice_path):
+    if not os.path.exists(slice_path):
+        im_slice = bead_stack[bead_stack.shape[0]//2]
+        # plt.imshow(im_slice)
+        # plt.show()
+        imwrite(slice_path, im_slice.astype(np.uint16))
+    return slice_path
+
+def get_or_create_locs(slice_path, args):
+    spots_path = slice_path.replace('.ome.tif', '.ome_spots.hdf5')
+    locs_path = slice_path.replace('.ome.tif', '.ome_locs.hdf5')
+
+    if not os.path.exists(spots_path) or not os.path.exists(locs_path) or args['regen']:
+        cmd = ['picasso', 'localize', slice_path, '-b', args['box_size_length'], '-g', args['gradient'], '-px', args['pixel_size']]
+        for extra_arg in ['qe', 'sensitivity', 'gain', 'baseline', 'fit-method']:
+            if extra_arg in args and args[extra_arg]:
+                cmd.extend([f'-{extra_arg}', args[extra_arg]])
+        cmd = ' '.join(list(map(str, cmd)))
+        tqdm.write('Running picasso...', end='')
+        res = subprocess.run(cmd, capture_output=True, shell=True, text=True)
+        if res.returncode != 0:
+            print('Picasso error occured')
+            print(res.stdout)
+            print(res.stderr)
+            return
+        tqdm.write('finished!')
+
+    with h5py.File(spots_path) as f:
+        spots = np.array(f['spots'])
+
+    locs = pd.read_hdf(locs_path, key='locs')
+    locs['fname'] = os.path.basename(slice_path)
+
+    return locs, spots
+
+def remove_colocal_locs(locs, spots, args):
+    tqdm.write('Removing overlapping locs...')
+    coords = locs[['x', 'y']].to_numpy()
+    dists = euclidean_distances(coords, coords)
+    np.fill_diagonal(dists, np.inf)
+    min_dists = dists.min(axis=1)
+    
+    error_margin = 0.8
+    min_seperation = (np.sqrt(2)  * args['box_size_length']) * error_margin
+    idx = np.argwhere(min_dists > min_seperation).squeeze()
+    locs = locs.iloc[idx]
+    spots = spots[idx]
+
+    return locs, spots
+
+
+def extract_training_stacks(spots, bead_stack, args) -> np.array:
+    spot_size = args['box_size_length']
+    frame_idx = bead_stack.shape[0]//2
+    frame = bead_stack[frame_idx]
+    stacks = []
+    for spot in spots:
+        res = match_template(frame, spot)
+        i, j = np.unravel_index(np.argmax(res), res.shape)
+        stack = bead_stack[:, i:i+spot_size, j:j+spot_size]
+        stacks.append(stack)
+    return np.array(stacks)
+
+def snr(psf):
+    return psf.max() / np.median(psf)
+
+
+def has_fwhm(psf):
+    psf = butterworth(psf, cutoff_frequency_ratio=0.2, high_pass=False)
+    y = np.max(gaussian(psf), axis=(1,2))
+    max_val = np.max(y)
+    min_val = np.min(y)
+    half_max = min_val + ((max_val-min_val) / 2)
+    crossCount = np.sum((y[:-1]>half_max) != (y[1:]>half_max))
+    if DEBUG and crossCount < 2:
+        plt.plot(y, label='raw')
+        plt.plot([0, len(y)], [half_max, half_max])
+        plt.show()
+    return crossCount >= 2
+
+
+def filter_beads(spots, locs, stacks, args):
+    print('Removing poorly imaged beads...', end='')
+    # Filter by SNR threshold
+    snrs = np.array([snr(psf) > args['min_snr'] for psf in stacks])
+    fwhms = np.array([has_fwhm(psf) for psf in stacks])
+
+    idx = np.argwhere(snrs & fwhms).squeeze()
+
+    spots = spots[idx]
+    locs = locs.iloc[idx]
+    stacks = stacks[idx]
+    print('finished!')
+
+    return spots, locs, stacks
+
+
+def write_combined_data(all_stacks, all_locs, args):
+    min_stack_length = min(list(map(lambda s: s.shape[1], all_stacks)))
+    stacks = [s[:, :min_stack_length] for s in all_stacks]
+    locs = pd.concat(all_locs)
+    stacks = np.concatenate(stacks)
+
+    outpath = os.path.join(args['outpath'], 'combined')
+    os.makedirs(outpath, exist_ok=True)
+
+    locs_outpath = os.path.join(outpath, 'locs.hdf')
+    stacks_outpath = os.path.join(outpath, 'stacks.ome.tif')
+
+    imwrite(stacks_outpath, stacks)
+    locs.to_hdf(locs_outpath, key='locs')
+    print('Saved results to:')
+    print(f'\t{locs_outpath}')
+    print(f'\t{stacks_outpath}')
+    print(f'Total beads: {locs.shape[0]}')
+
+
+def main(args):
+    all_stacks = []
+    all_spots = []
+    all_locs = []
+    for bead_stack_path in tqdm(args['bead_stacks']):
+        tqdm.write(f'Preparing {os.path.basename(bead_stack_path)}')
+
+        bead_stack = imread(bead_stack_path)
+        slice_path = bead_stack_path.replace('.ome', '_slice.ome')
+        slice_path = get_or_create_slice(bead_stack, slice_path)
+
+        locs, spots = get_or_create_locs(slice_path, args)
+        locs, spots = remove_colocal_locs(locs, spots, args)
+
+        stacks = extract_training_stacks(spots, bead_stack, args)
+
+        spots, locs, stacks = filter_beads(spots, locs, stacks, args)
+        tqdm.write(f'Retained {stacks.shape[0]} beads')
+        all_stacks.append(stacks)
+        all_spots.append(spots)
+        all_locs.append(locs)
+
+    write_combined_data(all_stacks, all_locs, args)
+        
+
+
+
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='')
+    parser.add_argument('bead_stacks', help='Path to TIFF bead stacks / directory containing bead stacks.')
+    parser.add_argument('-px', '--pixel_size', help='Pixel size (nm)', default=86, type=int)
+    parser.add_argument('-g', '--gradient', help='Min. net gradient', default=1000, type=int)
+    parser.add_argument('-b', '--box-size-length', help='Box size', default=15, type=int)
+    parser.add_argument('-qe', '--qe', help='Quantum efficiency', type=float)
+    parser.add_argument('-s', '--sensitivity', help='Sensitivity', type=float)
+    parser.add_argument('-ga', '--gain', help='Gain', type=float)
+    parser.add_argument('-bl', '--baseline', help='Baseline', type=int)
+    parser.add_argument('-a', '--fit-method', help='Fit method', choices=['mle', 'lq', 'avg'])
+    parser.add_argument('--regen', action='store_true')
+    parser.add_argument('-snr', '--min-snr', type=float, default=2.0)
+    parser.add_argument('--debug', action='store_true')
+
+    args = vars(parser.parse_args())
+
+    if args['debug']:
+        DEBUG = True
+
+    test_picasso_exec()
+    args = transform_args(args)
+    validate_args(args)
+    main(args)
