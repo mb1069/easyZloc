7fedd38a91050e21aee43b2fad72c9fc29372268
Miguel Boland <migueldboland@gmail.com>
2024-04-12 14:57:31 +0100

    Backup

--- publication/gen_all_openframe.sh
+++ publication/gen_all_openframe.sh
@@ -1,37 +1,42 @@
 # Openframe
-BEADS_DIR="/home/miguel/Projects/data/all_openframe_beads";
+BEADS_DIR="/home/miguel/Projects/data/20230601_MQ_celltype/20230601_MQ_celltype_beads";
 OUTDIR="/home/miguel/Projects/smlm_z/publication/VIT_openframe";
 PX_SIZE=86;
 ZSTEP=10;
-LOCS_NUP='/home/miguel/Projects/data/20230601_MQ_celltype/nup/fov2/storm_1/storm_1_MMStack_Default.ome_locs_undrifted.hdf5';
+LOCS_NUP='/home/miguel/Projects/data/20230601_MQ_celltype/nup/fov2/storm_1/storm_1_MMStack_Default.ome_locs.hdf5';
 SPOTS_NUP='/home/miguel/Projects/data/20230601_MQ_celltype/nup/fov2/storm_1/storm_1_MMStack_Default.ome_spots.hdf5';
 PICKED_LOCS_NUP='/home/miguel/Projects/data/20230601_MQ_celltype/nup/fov2/storm_1/storm_1_MMStack_Default.ome_locs_undrifted_picked_4.hdf5';
 
-source ~/anaconda3/etc/profile.d/conda.sh
+BRIGHTNESS=0
+GAUSS=0
 
+
+source ~/anaconda3/etc/profile.d/conda.sh
+MODEL_DIR=$OUTDIR/out_roll_alignment
+OUT_NUP=$OUTDIR/out_roll_alignment/out_nup
 # echo "Preparing data...";
 # conda activate picasso;
-# echo python3 /home/miguel/Projects/smlm_z/publication/prep_data.py $BEADS_DIR -z $ZSTEP -px $PX_SIZE --debug;
-# mkdir $OUTDIR;
-# cp $BEADS_DIR/combined/* $OUTDIR/;
+# echo python3 ~/publication/prep_data.py $BEADS_DIR -z $ZSTEP -px $PX_SIZE --debug;
+mkdir -p $OUTDIR;
+cp $BEADS_DIR/combined/* $OUTDIR/;
 
+set -e 
 
 conda activate smlm_z;
 
-# echo "Training model...";
-# cd $OUTDIR && echo $PWD &&
-# python3  /home/miguel/Projects/smlm_z/publication/train_model_debug.py -o out_roll_alignment --aug_ratio 2 --brightness 0.01 --gauss 0.001;
+echo "Training model...";
+cd $OUTDIR && echo $PWD &&
+python3 /home/miguel/Projects/smlm_z/publication/train_model.py --norm frame --system openframe --dataset $BEADS_DIR -o $MODEL_DIR --aug-brightness $BRIGHTNESS --aug-gauss $GAUSS;
 
-# echo "Localising NUP data..."
+python3 /home/miguel/Projects/smlm_z/publication/ -mo $MODEL_DIR --norm frame --zstep 10 --datasets /home/miguel/Projects/data/20231020_20nm_beads_10um_range_10nm_step /home/miguel/Projects/data/20230601_MQ_celltype/20230601_MQ_celltype_beads /home/miguel/Projects/data/all_openframe_beads/20231205_miguel_mitochondria /home/miguel/Projects/data/all_openframe_beads/20231212_miguel_openframe /home/miguel/Projects/data/20231128_tubulin_miguel
+
+echo "Localising NUP data..."
+cd $MODEL_DIR && echo $PWD &&
+python3 ~/publication/localise_exp_sample.py -l $LOCS_NUP -s $SPOTS_NUP -p $PICKED_LOCS_NUP -px $PX_SIZE -mo . -o $OUT_NUP;
+cd $OUT_NUP && echo $PWD &&
+python3 ~/publication/render_nup.py -l $OUT_NUP/locs_3d.hdf5 -px $PX_SIZE -df -os 10 -k 0.5 -df;
 # cd $OUTDIR/out_roll_alignment && echo $PWD &&
-# python3 /home/miguel/Projects/smlm_z/publication/localise_exp_sample.py -l $LOCS_NUP -s $SPOTS_NUP -p $PICKED_LOCS_NUP -mo . -o out_nup;
-# cd $OUTDIR/out_roll_alignment/out_nup && echo $PWD &&
-# python3 /home/miguel/Projects/smlm_z/publication/render_nup.py -l $OUTDIR/out_roll_alignment/out_nup/locs_3d.hdf5 -px $PX_SIZE -p $PICKED_LOCS_NUP;
-
-echo "Localising entire NUP data...";
-cd $OUTDIR/out_roll_alignment && echo $PWD &&
-python3 /home/miguel/Projects/smlm_z/publication/localise_exp_sample.py -l $LOCS_NUP -s $SPOTS_NUP -px $PX_SIZE -mo . -o out_nup_whole;
-
+# python3 ~/publication/localise_exp_sample.py -l $LOCS_NUP -s $SPOTS_NUP -px $PX_SIZE -mo . -o out_nup_full_fov;
 
 
 # LOCS_MITO="/media/Data/smlm_z_data/20231212_miguel_openframe/mitochondria/FOV2/storm_1/storm_1_MMStack_Default.ome_locs_undrift.hdf5";
@@ -40,7 +45,7 @@ python3 /home/miguel/Projects/smlm_z/publication/localise_exp_sample.py -l $LOCS
 
 # echo "Localising mitochondria data..."
 # cd $OUTDIR/out_roll_alignment && echo $PWD &&
-# python3 /home/miguel/Projects/smlm_z/publication/localise_exp_sample.py -l $LOCS_MITO -s $SPOTS_MITO -px $PX_SIZE -p $PICKED_MITO -mo . -o out_mitochondria;
+# python3 ~/publication/localise_exp_sample.py -l $LOCS_MITO -s $SPOTS_MITO -px $PX_SIZE -p $PICKED_MITO -mo . -o out_mitochondria;
 
 
 # LOCS_TUB="/media/Data/smlm_z_data/20231212_miguel_openframe/tubulin/FOV1/storm_1/storm_1_MMStack_Default.ome_locs_undrifted.hdf5";
@@ -48,4 +53,4 @@ python3 /home/miguel/Projects/smlm_z/publication/localise_exp_sample.py -l $LOCS
 # PICKED_TUB="/media/Data/smlm_z_data/20231212_miguel_openframe/tubulin/FOV1/storm_1/storm_1_MMStack_Default.ome_locs_undrifted_picked.hdf5";
 # echo "Localising tubulin data..."
 # cd $OUTDIR/out_roll_alignment && echo $PWD &&
-# python3 /home/miguel/Projects/smlm_z/publication/localise_exp_sample.py -l $LOCS_TUB -s $SPOTS_TUB -px $PX_SIZE -p $PICKED_TUB -mo . -o out_tubulin;
+# python3 ~/publication/localise_exp_sample.py -l $LOCS_TUB -s $SPOTS_TUB -px $PX_SIZE -p $PICKED_TUB -mo . -o out_tubulin;
